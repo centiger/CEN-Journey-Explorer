6e784969f27bf88eeacf9383b31a8379c03c66e8
centiger <centiger2@gmail.com>
2026-07-11 10:22:17 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'cen-journey-engine-v1.7.6';
+const CACHE = 'cen-journey-engine-v1.7.7';
 const CORE = [
   './',
   './index.html',
@@ -17,6 +17,7 @@ const CORE = [
   './journeys/OT/J006-wilderness-training-journey.json',
   './journeys/OT/J007-joshua-conquest-journey.json',
   './journeys/OT/J008-judges-cycle-journey.json',
+  './journeys/OT/J009-samuel-calling-kingship-journey.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   './assets/icons/icon-512-maskable.png'
